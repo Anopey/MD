099f86b463742bd1c903bc0e609faecba185b1ea
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-29 23:21:52 +0300

    start client unity project

---
+++ Client/MD/MD/Assets/ButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void Press(int whichPad)
+    {
+        Debug.Log("Pressed pad:" + whichPad);
+    }
+    public Slider slide;
+    public static int atWhichOne;
+    public void GetSliderPos()
+    {
+        atWhichOne = (int)slide.value;
+    }
+}
